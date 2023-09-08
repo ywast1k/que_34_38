6625b40be9860eca8c1b688446084a9ecf4c2aa5
Konstantin <kbynevskij@yandex.ru>
2023-09-08 23:33:33 +0200

    полный косяк

---
+++ 34_Que/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+int InputNun(string message){
+    
+Console.WriteLine(message);
+return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size){
+    return new int[size];
+}
+
+void FillArray(int[] arr, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i<arr.Length; i++)
+        arr[i]=rnd.Next(minValue,maxValue + 1);
+}
+
+
+
+
+
+ void PrintArray(int[] arr)
+ {
+     for(int i = 0; i < arr.Length; i++)
+     {
+         Console.Write(arr[i] + " ");
+     }
+     Console.WriteLine();
+ }
+
+ void PrintArrayTwo(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int count = 0;
+        if (arr[i] % 2 == 0)
+        Console.Write(arr[i]+" ");
+        
+       count++;
+    }
+  
+}
+
+
+
+int size = InputNun("Введите размер массива: ");
+int min = InputNun("Введите минимальное значение массива: ");
+int max = InputNun("Введите максимальное значение массива: ");
+
+int[] myArray = CreateArray(size);
+FillArray(myArray, min, max);
+PrintArray(myArray);
+PrintArrayTwo(myArray);
---
+++ 36_Que/Program.cs
@@ -0,0 +1,50 @@
+﻿//   Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//   [3, 7, 23, 12] -> 19
+//   [-4, -6, 89, 6] -> 0
+
+int InputNun(string message){
+    
+Console.WriteLine(message);
+return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size){
+    return new int[size];
+}
+
+void FillArray(int[] arr, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i<arr.Length; i++)
+    {
+        arr[i]=rnd.Next(minValue,maxValue + 1);
+        Console.Write(arr[i] + " ");}
+}
+
+int SummaElements(int[] arr){
+
+int sumEl = 0;
+for (int i = 0; i <arr.Length; i++ ){
+             if (i % 2 != 1)
+            {
+        sumEl = sumEl + arr[i];
+            }
+         {
+         return sumEl;
+         }
+  }
+}
+
+
+int size = InputNun("Введите размер массива: ");
+int min = InputNun("Введите минимальное значение массива: ");
+int max = InputNun("Введите максимальное значение массива: ");
+int[] myArray = CreateArray(size);
+FillArray(myArray, min, max);
+SummaElements(myArray);
+int sumEl =SummaElements(myArray);
+
+Console.Write("сумма: " + sumEl );
+
+
+
